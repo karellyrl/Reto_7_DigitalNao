e509973bf47593c500fd4cf941f4a3c29343d6bd
karellyrl <74926587+karellyrl@users.noreply.github.com>
2024-10-09 07:49:57 -0600

    Update app.module.ts

--- biblioteca-api/src/app.module.ts
+++ biblioteca-api/src/app.module.ts
@@ -2,33 +2,36 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { LibroModule } from './libro/libro.module';
 import { UsuarioModule } from './usuario/usuario.module';
-import { AuthModule } from './auth/auth.module'; // Asegúrate de importar el módulo de autenticación
-import { ReservaModule } from './reserva/reserva.module'; // Importa el módulo de reservas
-import { ReseñaModule } from './reseña/reseña.module'; // Importa el módulo de reseñas
+import { AuthModule } from './auth/auth.module';
+import { ReservaModule } from './reserva/reserva.module';
+import { ReseñaModule } from './reseña/reseña.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config(); // Cargar las variables de entorno desde .env
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'Rivera315.',
-      database: 'biblioteca',
+      host: process.env.DB_HOST, // Usa la variable de entorno
+      port: +process.env.DB_PORT, // Convierte a número
+      username: process.env.DB_USERNAME, // Usa la variable de entorno
+      password: process.env.DB_PASSWORD, // Usa la variable de entorno
+      database: process.env.DB_NAME, // Usa la variable de entorno
       entities: [
         __dirname + '/libro/entities/*.entity{.ts,.js}',
         __dirname + '/usuario/entities/*.entity{.ts,.js}',
-        __dirname + '/reserva/entities/*.entity{.ts,.js}', // Agrega la ruta para las entidades de reserva
-        __dirname + '/reseña/entities/*.entity{.ts,.js}', // Agrega la ruta para las entidades de reseña
+        __dirname + '/reserva/entities/*.entity{.ts,.js}',
+        __dirname + '/reseña/entities/*.entity{.ts,.js}',
       ],
       autoLoadEntities: true,
-      synchronize: true, // No usar en producción
+      synchronize: true,
     }),
     LibroModule,
     UsuarioModule,
-    AuthModule, // Asegúrate de que el módulo de autenticación esté aquí
-    ReservaModule, // Agrega el módulo de reservas
-    ReseñaModule, // Agrega el módulo de reseñas
+    AuthModule,
+    ReservaModule,
+    ReseñaModule,
   ],
 })
 export class AppModule {}

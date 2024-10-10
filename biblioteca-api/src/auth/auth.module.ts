b40b60ae3038d58913ef74737de3326a3e056192
karellyrl <74926587+karellyrl@users.noreply.github.com>
2024-10-10 02:30:49 -0600

    Update auth.module.ts

--- biblioteca-api/src/auth/auth.module.ts
+++ biblioteca-api/src/auth/auth.module.ts
@@ -1,24 +1,25 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { UsuarioModule } from '../usuario/usuario.module';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
-import { JwtStrategy } from './jwt.strategy';
-import { JwtAuthGuard } from './jwt-auth.guard';
+import { UsuarioModule } from '../usuario/usuario.module'; 
+import { AuthService } from './auth.service'; 
+import { AuthController } from './auth.controller'; 
+import { JwtStrategy } from './jwt.strategy'; 
+import { JwtAuthGuard } from './jwt-auth.guard'; 
 import * as dotenv from 'dotenv';
 
+// Cargar las variables de entorno desde el archivo .env
 dotenv.config();
 
 @Module({
   imports: [
-    UsuarioModule,
+    UsuarioModule, // Módulo de usuarios, usado para acceder a los servicios de usuario
     JwtModule.register({
-      secret: process.env.JWT_SECRET, // Usa la variable de entorno
-      signOptions: { expiresIn: '1h' },
+      secret: process.env.JWT_SECRET, // Clave secreta usada para firmar y verificar JWT, obtenida de las variables de entorno
+      signOptions: { expiresIn: '1h' }, // El token JWT expira en 1 hora
     }),
   ],
-  providers: [AuthService, JwtStrategy, JwtAuthGuard],
-  controllers: [AuthController],
-  exports: [JwtAuthGuard],
+  providers: [AuthService, JwtStrategy, JwtAuthGuard], // Proveedores disponibles dentro del módulo: servicio de autenticación, estrategia JWT y guard JWT
+  controllers: [AuthController], // Controlador de autenticación para manejar las rutas
+  exports: [JwtAuthGuard], // Exporta el guard JWT para que pueda ser usado en otros módulos
 })
 export class AuthModule {}

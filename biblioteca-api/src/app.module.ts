68e808c1ef90d18c2736bf55a0284fa180bbb031
karellyrl <74926587+karellyrl@users.noreply.github.com>
2024-10-10 02:34:39 -0600

    Update app.module.ts

--- biblioteca-api/src/app.module.ts
+++ biblioteca-api/src/app.module.ts
@@ -1,37 +1,39 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { LibroModule } from './libro/libro.module';
-import { UsuarioModule } from './usuario/usuario.module';
-import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm'; 
+import { LibroModule } from './libro/libro.module'; 
+import { UsuarioModule } from './usuario/usuario.module'; 
+import { AuthModule } from './auth/auth.module'; 
 import { ReservaModule } from './reserva/reserva.module';
-import { ReseñaModule } from './reseña/reseña.module';
+import { ReseñaModule } from './reseña/reseña.module'; 
 import * as dotenv from 'dotenv';
 
-dotenv.config(); // Cargar las variables de entorno desde .env
+// Cargar las variables de entorno desde el archivo .env
+dotenv.config();
 
 @Module({
   imports: [
+    // Configuración del TypeORM con MySQL
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST, // Usa la variable de entorno
-      port: +process.env.DB_PORT, // Convierte a número
-      username: process.env.DB_USERNAME, // Usa la variable de entorno
-      password: process.env.DB_PASSWORD, // Usa la variable de entorno
-      database: process.env.DB_NAME, // Usa la variable de entorno
+      type: 'mysql', 
+      host: process.env.DB_HOST, 
+      port: +process.env.DB_PORT, 
+      username: process.env.DB_USERNAME, 
+      password: process.env.DB_PASSWORD, 
+      database: process.env.DB_NAME, 
       entities: [
-        __dirname + '/libro/entities/*.entity{.ts,.js}',
-        __dirname + '/usuario/entities/*.entity{.ts,.js}',
-        __dirname + '/reserva/entities/*.entity{.ts,.js}',
-        __dirname + '/reseña/entities/*.entity{.ts,.js}',
+        __dirname + '/libro/entities/*.entity{.ts,.js}', // Entidades de libros
+        __dirname + '/usuario/entities/*.entity{.ts,.js}', // Entidades de usuarios
+        __dirname + '/reserva/entities/*.entity{.ts,.js}', // Entidades de reservas
+        __dirname + '/reseña/entities/*.entity{.ts,.js}', // Entidades de reseñas
       ],
-      autoLoadEntities: true,
-      synchronize: true,
+      autoLoadEntities: true, // Carga automáticamente las entidades registradas
+      synchronize: true, // Sincroniza automáticamente las entidades con la base de datos 
     }),
-    LibroModule,
-    UsuarioModule,
-    AuthModule,
-    ReservaModule,
-    ReseñaModule,
+    LibroModule, 
+    UsuarioModule, 
+    AuthModule, 
+    ReservaModule, 
+    ReseñaModule, 
   ],
 })
 export class AppModule {}

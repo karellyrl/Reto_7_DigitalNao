2a51028e1077f309b805a3edc51c8a4f5fed375e
karellyrl <74926587+karellyrl@users.noreply.github.com>
2024-10-10 02:22:01 -0600

    Update main.ts

--- biblioteca-api/src/main.ts
+++ biblioteca-api/src/main.ts
@@ -3,34 +3,40 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
+  // Crear la aplicación de NestJS
   const app = await NestFactory.create(AppModule);
 
-  // Habilitar CORS
+  // Habilitar CORS para permitir solicitudes desde otros dominios
   app.enableCors({
-    origin: '*', // Permitir todas las orígenes, puedes restringirlo a un dominio específico
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    origin: '*', // Permitir todas las orígenes, puedes restringirlo a un dominio específico en producción
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Métodos permitidos en las solicitudes
+    allowedHeaders: ['Content-Type', 'Authorization'], // Headers permitidos en las solicitudes
   });
 
-  // Configuración de Swagger
+  // Configuración de Swagger para la documentación de la API
   const config = new DocumentBuilder()
-    .setTitle('Biblioteca API')
-    .setDescription('API para gestionar libros, usuarios, reseñas y reservas')
-    .setVersion('1.0')
-    .addTag('Libros')
-    .addTag('Usuarios')
-    .addTag('Reseñas')
-    .addTag('Reservas')
-    .addTag('Login')
-    .addBearerAuth() // Añadir esquema de autenticación Bearer JWT
-    .addServer('https://reto-7-digitalnao.onrender.com') // Cambiar esto a la URL de producción
+    .setTitle('Biblioteca API') // Título del documento Swagger
+    .setDescription('API para gestionar libros, usuarios, reseñas y reservas') // Descripción del API
+    .setVersion('1.0') // Versión de la API
+    .addTag('Libros') // Categoría para operaciones relacionadas con libros
+    .addTag('Usuarios') // Categoría para operaciones relacionadas con usuarios
+    .addTag('Reseñas') // Categoría para operaciones relacionadas con reseñas
+    .addTag('Reservas') // Categoría para operaciones relacionadas con reservas
+    .addTag('Login') // Categoría para operaciones relacionadas con el login
+    .addBearerAuth() // Esquema de autenticación Bearer JWT para proteger las rutas
+    .addServer('https://reto-7-digitalnao.onrender.com') // URL del servidor en producción, puede cambiar según el despliegue
     .build();
 
+  // Crear el documento Swagger a partir de la configuración anterior
   const document = SwaggerModule.createDocument(app, config);
+
+  // Configurar Swagger en la ruta /api para que sea accesible desde esa URL
   SwaggerModule.setup('api', app, document);
 
-  // Cambiar esta línea para usar la variable de entorno PORT
+  // Configuración del puerto, usando la variable de entorno PORT o el puerto 3000 por defecto
   const port = process.env.PORT || 3000;
+  
+  // Iniciar la aplicación en el puerto configurado
   await app.listen(port);
 }
 

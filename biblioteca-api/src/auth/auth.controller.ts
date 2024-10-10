e385d425b8cb17c2e1a94a0cdf9341b8d9b0a4ff
karellyrl <74926587+karellyrl@users.noreply.github.com>
2024-10-10 02:28:58 -0600

    Update auth.controller.ts

--- biblioteca-api/src/auth/auth.controller.ts
+++ biblioteca-api/src/auth/auth.controller.ts
@@ -1,20 +1,26 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { Usuario } from '../usuario/entities/usuario.entity';
-import { CreateUsuarioDto } from '../usuario/entities/usuario.dto'; // Asegúrate de importar el DTO correspondiente
-import { ApiTags } from '@nestjs/swagger';
+import { Usuario } from '../usuario/entities/usuario.entity'; 
+import { CreateUsuarioDto } from '../usuario/entities/usuario.dto'; 
+import { ApiTags } from '@nestjs/swagger'; 
 
-@ApiTags('Login')
-@Controller('auth')
+@ApiTags('Login') // Etiqueta la categoría de "Login" en la documentación de Swagger
+@Controller('auth') // Define que este controlador maneja rutas bajo '/auth'
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) {} // Inyecta el servicio de autenticación
 
+  // Maneja la solicitud POST para la ruta '/auth/login'
   @Post('login')
   async login(@Body() usuario: CreateUsuarioDto) {
+    // Llama al servicio de autenticación para validar las credenciales del usuario
     const validUsuario = await this.authService.validateUsuario(usuario.email, usuario.password);
+    
+    // Si las credenciales no son válidas, devuelve un mensaje de error
     if (!validUsuario) {
-      return { message: 'Invalid credentials' };
+      return { message: 'Invalid credentials' }; // Puedes lanzar una excepción en lugar de devolver un objeto
     }
+    
+    // Si las credenciales son válidas, llama al servicio para generar y devolver el token JWT
     return this.authService.login(validUsuario);
   }
 }
